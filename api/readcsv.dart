9ef140f92f4eb554a3410ff992cb0a25e49d7d51
pxai <pello_altadill@cuatrovientos.org>
2016-11-09 22:41:05 +0100

    added csv processor sample

--- api/readcsv.dart
+++ api/readcsv.dart
@@ -3,7 +3,8 @@ import 'dart:io';
 import 'dart:convert';
 
 main() {
-  var path = '../sampledata/people.csv';
+  //var path = '../sampledata/people.csv';
+  var path = 'c:/code/tmp/idiomas.csv';
     new File(path)
     .openRead()
     .transform(UTF8.decoder)
@@ -16,7 +17,24 @@ main() {
 
 convert (line) {
   var data = line.toString().split(';');
-  for(int i=0;i< data.length;i++) {
-    print(data[i]);
+  var names = data[2].split(',');
+  var email = data[4].trim();
+  var group = data[0].trim();
+
+  print(format(names[2]) + ';' + format(names[0]) +';' + format(names[1]) + ';' + group + ';' +email );
+}
+
+format (String name) {
+  name = name.toLowerCase().trim();
+  var result = name[0].toUpperCase();
+  var previous = '';
+
+  for (int i = 1; i<name.length;i++) {
+    if (previous != ' ') {
+      result += previous = name[i].toLowerCase();
+    } else {
+      result += previous = name[i].toUpperCase();
+    }
   }
+  return result;
 }
